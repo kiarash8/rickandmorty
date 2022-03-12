e70cb90972ddfb77f15d5f3b6d43874c3375f441
kiarash fereiduni <f.kiarash8@gmail.com>
2022-03-13 02:08:35 +0330

    deploy issue

--- src/App.tsx
+++ src/App.tsx
@@ -15,21 +15,21 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <Router>
           <Routes>
-            {Layouts.map((layout, index) =>
-              <Route
-                key={index}
-                path={layout.path}
-                element={layout.component}
-              >
-                {routes.filter(x=> x.layout === layout.name).map((item, key) => 
-                  <Route
-                    key={key}
-                    path={item.path}
-                    element={item.component}
-                  />
-                )}
-              </Route>
-            )}
+              {Layouts.map((layout, index) =>
+                <Route
+                  key={index}
+                  path={`rickandmorty${layout.path}`}
+                  element={layout.component}
+                >
+                  {routes.filter(x=> x.layout === layout.name).map((item, key) => 
+                    <Route
+                      key={key}
+                      path={item.path}
+                      element={item.component}
+                    />
+                  )}
+                </Route>
+              )}
           </Routes>
       </Router>
     </QueryClientProvider>
